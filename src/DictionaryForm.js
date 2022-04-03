73bc8209ac26386b3d2f9de2dccfa8df935c2261
sarah-ross <sarahross025@gmail.com>
2022-04-03 16:51:14 -0400

    Update DictionaryForm.js

--- src/DictionaryForm.js
+++ src/DictionaryForm.js
@@ -18,6 +18,7 @@ export default function DictionaryForm() {
 	function search(event) {
 		event.preventDefault();
 
+		// documentation link: https://dictionaryapi.dev/
 		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 		axios.get(apiUrl).then(handleResponse);
 	}
