95211e2a8770f4937aae3dd5d65ee4eb23ad5366
sarah-ross <sarahross025@gmail.com>
2022-04-02 23:12:30 -0400

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<div className="App container">
+			<h1 className="mt-5">React Dictionary App</h1>
+			<h3>Look up a word:</h3>
+		</div>
+	);
 }
-
-export default App;
