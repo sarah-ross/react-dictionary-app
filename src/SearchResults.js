3ab595f420c760c082dd4789436be167345df79c
sarah-ross <sarahross025@gmail.com>
2022-04-09 22:22:18 -0400

    Added Phonetics Component

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 import "./SearchResults.css";
 
@@ -9,6 +10,16 @@ export default function SearchResults(props) {
 			<div className="SearchResults">
 				<div className="container results-wrapper shadow">
 					<h2>{props.results.word}</h2>
+					{props.results.phonetics.map(function (
+						phonetic,
+						index
+					) {
+						return (
+							<div key={index}>
+								<Phonetic phonetic={phonetic} />
+							</div>
+						);
+					})}
 					{props.results.meanings.map(function (
 						meaning,
 						index
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import "./Synonyms.css";
+
 export default function Synonyms(props) {
 	console.log(props);
 	if (props.synonyms && props.synonyms.length > 0) {
 		return (
 			<ul className="Synonyms">
+				<span>Synonyms:</span>
 				{props.synonyms.map(function (synonym, index) {
 					return <li key={index}>{synonym}</li>;
 				})}
