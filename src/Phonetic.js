38bc77eace12e7051689142e6850b98e448f3217
sarah-ross <sarahross025@gmail.com>
2022-04-09 22:29:57 -0400

    Updated Phonetic.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function Phonetic(props) {
 	return (
 		<div className="Phonetic">
-			<a href={props.phonetic.audio} target="_blank">
+			<a
+				href={props.phonetic.audio}
+				target="_blank"
+				rel="noreferrer">
 				Listen
 			</a>
 			<br />
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -9,25 +9,27 @@ export default function SearchResults(props) {
 		return (
 			<div className="SearchResults">
 				<div className="container results-wrapper shadow">
-					<h2>{props.results.word}</h2>
-					{props.results.phonetics.map(function (
-						phonetic,
-						index
-					) {
-						return (
-							<div key={index}>
-								<Phonetic phonetic={phonetic} />
-							</div>
-						);
-					})}
+					<section>
+						<h2>{props.results.word}</h2>
+						{props.results.phonetics.map(function (
+							phonetic,
+							index
+						) {
+							return (
+								<div key={index}>
+									<Phonetic phonetic={phonetic} />
+								</div>
+							);
+						})}
+					</section>
 					{props.results.meanings.map(function (
 						meaning,
 						index
 					) {
 						return (
-							<div key={index}>
+							<section key={index}>
 								<Meaning meaning={meaning} />
-							</div>
+							</section>
 						);
 					})}
 				</div>
